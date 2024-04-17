b2e31bb2465a7fac96745caba908430000277163
Sumedh Pawar <sumedhpawar8966@gmail.com>
2024-04-18 01:06:18 +0530

    changed cors config

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,10 @@
 const corsConfig = {
-  origin: ["http://localhost:5173", process.env.CLIENT_URL],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
